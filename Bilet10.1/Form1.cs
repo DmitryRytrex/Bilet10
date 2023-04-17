8160f7a61532d175db6ba8b847156ab6d7d2bea4
Solovov <Solovov@cchgeu.ru>
2023-04-17 15:36:48 +0300

    Добавьте файлы проекта.

---
+++ Bilet10.1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Bilet10._1
+{
+    public partial class Form1 : Form
+    {
+        public void loaddata()
+        {
+            using (ConnectDB db = new ConnectDB())
+            {
+                DataTable users = db.ExecuteSql("Select * from contacts");
+                dataGridView1.DataSource = users;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FormCreate create = new FormCreate();
+            create.Show();
+            loaddata();
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            loaddata();
+            dataGridView1.Update();
+            dataGridView1.Refresh();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (ConnectDB db = new ConnectDB())
+            {
+                int num = dataGridView1.CurrentCell.RowIndex;
+                db.ExecuteSqlNonQuery($"Delete from contacts where ID = {num+1};");
+            }
+            MessageBox.Show("Пользователь удалён!");
+            loaddata();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (ConnectDB db = new ConnectDB())
+            {
+                int num = dataGridView1.CurrentCell.RowIndex;
+                num++;
+                //db.ExecuteSqlNonQuery($"Update contacts set column2 = {Name}, column3 = {Surname}, column4 = {patronymic}, column5 = {} ");
+            }    
+                loaddata();
+        }
+    }
+}
---
+++ Bilet10.1/FormCreate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bilet10._1
+{
+    public partial class FormCreate : Form
+    {
+        public FormCreate()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            using (ConnectDB db = new ConnectDB())
+            {
+                DataTable dt = db.ExecuteSql($"Select * from contacts where phone_number = '{textBox4.Text}';");
+                if (dt.Rows.Count > 0)
+                {
+                    MessageBox.Show("Пользователь с таким номером телефона уже существует!");
+                }
+                else
+                {
+                    db.ExecuteSqlNonQuery($"insert into contacts values('{textBox1.Text}', '{textBox2.Text}', '{textBox3.Text}', '{textBox4.Text}', '{textBox5.Text}');");
+                    MessageBox.Show("Пользователь успешно добавлен!");
+                    Form1 main = new Form1();
+                    main.loaddata();
+                    main.Update();
+                    main.Refresh();
+                    this.Close();
+                }
+            }
+        }
+    }
+}
